d20247fee15c5a5b81a731ffeacc5cd2e35d9738
Brolgens <93027478+Brolgens@users.noreply.github.com>
2022-08-26 20:42:05 +0200

    Initial commit

---
+++ src/main/java/net/brolgens/bettervanilla/BetterVanillaClient.java
@@ -0,0 +1,10 @@
+package net.brolgens.bettervanilla;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterVanillaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
