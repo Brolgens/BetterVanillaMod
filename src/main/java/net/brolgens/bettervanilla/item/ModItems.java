bb55918f656b71d3fd900c1ce16adf937f64adc6
Brolgens <93027478+Brolgens@users.noreply.github.com>
2022-08-31 21:46:17 +0200

    Added first fully working Copper Tools
    Signed-off-by: Brolgens <93027478+Brolgens@users.noreply.github.com>

--- src/main/java/net/brolgens/bettervanilla/item/ModItems.java
+++ src/main/java/net/brolgens/bettervanilla/item/ModItems.java
@@ -1,26 +1,46 @@
 package net.brolgens.bettervanilla.item;
 
 import net.brolgens.bettervanilla.BetterVanilla;
+import net.brolgens.bettervanilla.item.custom.CustomAxeItem;
+import net.brolgens.bettervanilla.item.custom.CustomPickaxeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.brolgens.bettervanilla.item.custom.CustomHoeItem;
 
 public class ModItems {
 
     public static final Item COPPER_SWORD = registerItem("copper_sword",
-            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+            new SwordItem(ModToolMaterial.COPPER, 3, -2.4f,
+                    new FabricItemSettings().group(ItemGroup.COMBAT)));
+
+    public static final Item COPPER_SHOVEL = registerItem("copper_shovel",
+            new ShovelItem(ModToolMaterial.COPPER, 2.5f, -3.0f,
+                    new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    public static final Item COPPER_HOE = registerItem("copper_hoe",
+            new CustomHoeItem(ModToolMaterial.COPPER, -1, -0.5f,
+                    new FabricItemSettings().group(ItemGroup.TOOLS)));
 
     public static final Item COPPER_AXE = registerItem("copper_axe",
-            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+            new CustomAxeItem(ModToolMaterial.COPPER, 6.5f, -3.0f,
+                    new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    public static final Item COPPER_PICKAXE = registerItem("copper_pickaxe",
+            new CustomPickaxeItem(ModToolMaterial.COPPER, 1, -2.8f,
+                    new FabricItemSettings().group(ItemGroup.TOOLS)));
 
 
-    private static Item registerItem(String name, Item item){
+    //public static final Item COPPER_AXE = registerItem("copper_axe",
+    //      new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+
+
+    private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(BetterVanilla.MOD_ID, name), item);
     }
 
-    public static void registerModItems(){
-        BetterVanilla.LOGGER.debug("Registering Mod Items for " + BetterVanilla.MOD_ID);
+    public static void registerModItems() {
+        BetterVanilla.LOGGER.debug("Registering Modded Items for " + BetterVanilla.MOD_ID);
     }
 }
